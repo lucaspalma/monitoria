368264ab8066ece1b770df205cd153606c9a313b
Lucas Takeshi Rodrigues Palma <lucastakeshipalma@gmail.com>
2014-03-21 12:26:17 -0300

    Validando o professor

--- src/main/java/br/usp/ime/monitoria/monitoria/controller/HomeController.java
+++ src/main/java/br/usp/ime/monitoria/monitoria/controller/HomeController.java
@@ -4,6 +4,7 @@ import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.Validator;
 import br.usp.ime.monitoria.monitoria.modelo.professor.Professor;
 import br.usp.ime.monitoria.monitoria.modelo.professor.ProfessorDao;
 
@@ -12,10 +13,12 @@ public class HomeController {
 	
 	private final Result result;
 	private ProfessorDao professorDao;
+	private Validator validator;
 
-	public HomeController(Result result, ProfessorDao professorDao) {
+	public HomeController(Result result, ProfessorDao professorDao, Validator validator) {
 		this.result = result;
 		this.professorDao = professorDao;
+		this.validator = validator;
 	}
 	
 	@Get("/")
@@ -25,7 +28,8 @@ public class HomeController {
 
 	@Post("/cadastraProfessor")
 	public void cadastraProfessor(Professor professor) {
-		//valida o professor
+		validator.validate(professor);
+		validator.onErrorForwardTo(this).home();
 		professorDao.add(professor);
 		result.include("professor", professor);
 	}
